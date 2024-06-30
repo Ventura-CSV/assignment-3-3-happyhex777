4ac3e1007d9bf4775ca00a4215e6378c9c7a71c4
Hector Diaz <hhectordiazz@icloud.com>
2024-06-29 23:53:47 -0700

    3-3 complete

--- main.py
+++ main.py
@@ -5,7 +5,25 @@ def main():
     Code Your Program here
     ########################################
     """
-    result = True
+    def is_valid_email(email):
+        result = False
+        
+        if not email[0].isalpha():
+            return result
+        
+        if not (5 < len(email) < 30):
+            return result
+        
+        if '@' not in email:
+            return result
+        
+        at_index = email.index('@')
+        if'.' not in email[at_index:]:
+            return result
+        result = True
+        return result
+    result = is_valid_email(email)
+    print(result)
 
     ########################################
     # Do not delete the return statement
